ca6588dad071c6530606e5de612c905a28bf185a
dshah374@gatech.edu <90000344+DhruvShah09@users.noreply.github.com>
2023-12-01 18:50:04 -0500

    Reverse Proxy Gateway Config

--- test_api.py
+++ test_api.py
@@ -1,11 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import subprocess
+import fileinput
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+NGINX_CONFIG_FILE = "/etc/nginx/sites-enabled/fastapi_nginx"
 
-@app.get("/greet/{name}")
-def greet(name: str):
-    return {"message": f"Hello, {name}!"}
+@app.post("/reload-nginx")
+async def reload_nginx(request_body: dict):
+    # Assuming request_body contains the new proxy data
+    new_proxy_data = request_body.get("new_proxy_data")
+
+    if not new_proxy_data:
+        raise HTTPException(status_code=400, detail="Missing 'new_proxy_data' in request body")
+
+    try:
+        # Replace the IP and port in the Nginx configuration file
+        replace_proxy_data(new_proxy_data)
+
+        # Send HUP signal to Nginx master process
+        subprocess.run(["sudo", "nginx", "-s", "reload"], check=True)
+
+        return {"message": "Nginx reloaded successfully"}
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=f"Failed to reload Nginx: {e}")
+
+def replace_proxy_data(new_proxy_data):
+    with fileinput.FileInput(NGINX_CONFIG_FILE, inplace=True, backup=".bak") as file:
+        for line in file:
+            if "proxy_pass" in line:
+                # Replace the IP and port in the proxy_pass line
+                line = f"        proxy_pass {new_proxy_data};\n"
+            print(line, end='')
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
